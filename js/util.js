bdd889f0858ade623b2911364b5382fd69430126
jl777 <jameslee777@yahoo.com>
2015-11-05 03:32:08 -0300

    test

--- js/util.js
+++ js/util.js
@@ -159,7 +159,7 @@ function readBlob(opt_startByte, opt_stopByte) {
 		
 		  var confString = e.target.result;
 		  
-		  postCall("SuperNET", '{\"requestType\":\"setconf\",\"jsonstr\": ' + confString + '}', function(result){
+		  postCall("SuperNET",confString, function(result){
 			console.log(result);
 		  });
 		
